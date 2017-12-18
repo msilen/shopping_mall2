8c72ba2a9b77d7ff2b3f4d29a2a39fcd631241de
danmyasnikov <daniel.g.myasnikov@gmail.com>
2017-12-18 12:24:35 +1100

    migrated user roles

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,4 +1,4 @@
 Spree::User.class_eval do
-  has_many :role_users, class_name: 'Spree::RolesUser'
+  has_many :role_users, class_name: 'Spree::RoleUser'
   has_many :roles, through: :role_users, class_name: 'Spree::Role'
 end
