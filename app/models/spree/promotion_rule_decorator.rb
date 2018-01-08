29ee250a98d68bf90460f8b3662e547a0e82df8d
danmyasnikov <daniel.g.myasnikov@gmail.com>
2018-01-08 15:18:25 +1100

    promotion rules to work with apartment gem

--- app/models/spree/promotion_rule_decorator.rb
+++ app/models/spree/promotion_rule_decorator.rb
@@ -2,5 +2,5 @@ Spree::PromotionRule.class_eval do
   has_many :product_promotion_rules
   has_many :promotion_rules,
            through: :product_promotion_rules,
-           class_name: 'Spree::ProductsPromotionRules'
+           class_name: 'Spree::ProductPromotionRules'
 end
