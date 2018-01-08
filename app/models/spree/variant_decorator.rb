eed19b27d679361490136ef6085933ca739a52ac
danmyasnikov <daniel.g.myasnikov@gmail.com>
2018-01-08 15:18:39 +1100

    variants to work with apartment gem

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Variant.class_eval do
-  has_many :option_value_variants, class_name: 'Spree::OptionValuesVariant'
+  has_many :option_value_variants, class_name: 'Spree::OptionValueVariant'
   has_many :option_values,
            through: :option_value_variants,
            class_name: 'Spree::OptionValue'
