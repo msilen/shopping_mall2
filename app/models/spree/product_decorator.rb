c06f350cba4662b2d88499f716dfce27fb88f291
Martin Silenus <sileenus@gmail.com>
2018-08-25 20:24:32 +0300

    change accociation order

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
-  has_many :product_promotion_rules, class_name: 'Spree::ProductPromotionRule'
   has_many :promotion_rules,
            through: :product_promotion_rules,
            class_name: 'Spree::PromotionRule'
+  has_many :product_promotion_rules, class_name: 'Spree::ProductPromotionRule'
 end
